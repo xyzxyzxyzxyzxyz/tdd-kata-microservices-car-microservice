80de54f065173cf5759cec9f93479b9e0e6d0b35
XYZ <idm.caz@mailinator.com>
2017-04-09 15:59:10 +0200

    Initial commit.

---
+++ src/main/java/com/tdd/katas/microservices/CarMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.katas.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMicroserviceApplication.class, args);
+	}
+}
